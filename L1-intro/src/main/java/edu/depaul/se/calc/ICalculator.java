7ed018261a5067628c2925269dd7c513c128f639
Ken Yu <kyu_23@yahoo.com>
2012-11-16 20:39:35 -0600

    Update for java 7 and WTP

---
+++ L1-intro/src/main/java/edu/depaul/se/calc/ICalculator.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.depaul.se.calc;
+
+/**
+ *
+ * @author oose
+ */
+public interface ICalculator {
+    public int add(int lhs, int rhs);
+}
