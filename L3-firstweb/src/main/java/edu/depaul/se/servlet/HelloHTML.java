61c7b977b1a7498d02d1fa5805640b26e8009763
Ken Yu <shinabro@gmail.com>
2018-03-17 15:56:37 -0700

    Updated to support JDK 9

--- L3-firstweb/src/main/java/edu/depaul/se/servlet/HelloHTML.java
+++ L3-firstweb/src/main/java/edu/depaul/se/servlet/HelloHTML.java
@@ -1,15 +1,14 @@
 package edu.depaul.se.servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-import javax.jws.soap.InitParam;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 @WebServlet(urlPatterns = {"/greenServlet"}, 
                 initParams={ @WebInitParam(name="bgcolor", value="green"), 
@@ -28,7 +27,7 @@ public class HelloHTML extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html>");
