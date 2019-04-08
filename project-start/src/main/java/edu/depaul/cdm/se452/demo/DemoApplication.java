0c4894da40779df2f2463971577195e329d8b957
Ken Yu <shinabro@gmail.com>
2019-04-08 06:44:50 -0500

    Updated main to use functional block

--- project-start/src/main/java/edu/depaul/cdm/se452/demo/DemoApplication.java
+++ project-start/src/main/java/edu/depaul/cdm/se452/demo/DemoApplication.java
@@ -1,6 +1,5 @@
 package edu.depaul.cdm.se452.demo;
 
-import edu.depaul.cdm.se452.demo.model.Flight;
 import edu.depaul.cdm.se452.demo.model.FlightRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,23 +11,23 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	
-	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	//@Bean
-	public CommandLineRunner demo(FlightRepository repository) {
-		return (args) -> {
-			// fetch all customers
-			log.info("Flights found with findAll():");
-			log.info("-------------------------------");
-			for (Flight flight : repository.findAll()) {
-				log.info(flight.toString());
-			}
-			log.info("");
-		};
-	}	
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+    @Bean
+    public CommandLineRunner demo(FlightRepository repository) {
+        return (args) -> {
+            // fetch all customers
+            log.info("Flights found with findAll():");
+            log.info("-------------------------------");
+            repository.findAll().forEach((flight) -> {
+                log.info(flight.toString());
+            });
+            log.info("");
+        };
+    }
 
 }
