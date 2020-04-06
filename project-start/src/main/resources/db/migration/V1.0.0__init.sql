80df1926295861e62f1c1e2c372b6d4ecfef16f0
Ken Yu <shinabro@gmail.com>
2020-04-05 21:12:56 -0500

    Update to latest spring boot version

--- project-start/src/main/resources/db/migration/V1.0.0__init.sql
+++ project-start/src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,8 +1,5 @@
 create table github_project (
-    id identity not null primary key,
+    github_id identity ,
     org_name varchar(50) not null,
-    repo_name varchar(50) not null unique,
+    repo_name varchar(50) not null
 );
-
-create index idx_repo_name
-    on github_project(repo_name);
