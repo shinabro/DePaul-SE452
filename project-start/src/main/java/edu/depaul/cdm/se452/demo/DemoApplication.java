575a3e945bebde6e1a30f8976f4997e9cda7acf7
Ken Yu <shinabro@gmail.com>
2020-04-11 18:50:02 -0500

    Update data and list

--- project-start/src/main/java/edu/depaul/cdm/se452/demo/DemoApplication.java
+++ project-start/src/main/java/edu/depaul/cdm/se452/demo/DemoApplication.java
@@ -1,6 +1,14 @@
 package edu.depaul.cdm.se452.demo;
 
+import edu.depaul.cdm.se452.demo.model.Airport;
+import edu.depaul.cdm.se452.demo.model.AirportRepository;
+import edu.depaul.cdm.se452.demo.model.Flight;
 import edu.depaul.cdm.se452.demo.model.FlightRepository;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,8 +25,34 @@ public class DemoApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
+    /**
+     * Demo of listing data
+     * @param repository
+     * @return 
+     */
     @Bean
-    public CommandLineRunner demo(FlightRepository repository) {
+    public CommandLineRunner demo(FlightRepository flightRepository, AirportRepository airportRepository) {
+        return (args) -> {
+            Airport originationAirport = airportRepository.findByCode("ORD");
+            Airport destinationAirport = airportRepository.findByCode("DEN");
+            
+            Flight flight2 = new Flight();
+            flight2.setFlightNumber("SW1234");
+            Calendar cal = new GregorianCalendar();
+            flight2.setArrivalDateTime(new Timestamp(cal.getTimeInMillis()));
+            flight2.setOriginationAirport(originationAirport);
+            flight2.setDestinationAirport(destinationAirport);
+            flightRepository.save(flight2);
+        };
+    }
+    
+    /**
+     * Demo of listing data
+     * @param repository
+     * @return 
+     */
+    @Bean
+    public CommandLineRunner showAllFlights(FlightRepository repository) {
         return (args) -> {
             // fetch all customers
             log.info("Flights found with findAll():");
